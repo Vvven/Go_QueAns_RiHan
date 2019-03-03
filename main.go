4751f607e84bb394fd8f4e567d67089d4748e820
Vvven <liangshuwen_163yx@163.com>
2019-03-04 00:23:04 +0800

    改为相对路径提交

---
+++ main.go
@@ -0,0 +1,33 @@
+﻿package main
+
+import (
+	//"database/sql"
+
+	//"encoding/json"
+	//"fmt"
+	//"io/ioutil"
+	//"log"
+	//"net/http"
+
+	_ "github.com/lib/pq"
+
+	"./psql"
+	//"./router"
+)
+
+func main() {
+	psql.Db = psql.ConnectDB()
+
+	psql.GetUserTopic(1)
+
+	// http.HandleFunc("/querytime", router.Query_test_time) //显示所有用户信息
+	// http.HandleFunc("/showuserinfo", router.ShowUserInfo)       // 个人信息
+	// http.HandleFunc("/showuserquelist", router.ShowUserQueList) // 个人提问列表
+	// http.HandleFunc("/showuseranslist", router.ShowUserAnsList) // 个人回答列表
+	// http.HandleFunc("/addtopics", router.AddTopics)             // 添加帖子
+
+	// err := http.ListenAndServe(":4000", nil)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServe: ", err)
+	//}
+}
---
+++ psql/test.go
@@ -0,0 +1,15 @@
+package psql
+
+// import (
+// 	"testing"
+// 	"fmt"
+// )
+
+// func test_GetUserTopic(t *testing.T) {
+// 	id:=Getid{
+// 		Userid :1,
+// 	}
+// 	ropic := GetUserTopic(id)
+// 	fmt.Println(ropic)
+// 	t.Error()
+// }
